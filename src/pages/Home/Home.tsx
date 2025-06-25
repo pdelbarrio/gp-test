695d62d5dbf4523e50b697d0661ebf3d8fc1aa87
Pablo del Barrio <pablodbb@gmail.com>
2025-06-25 18:40:52 +0200

    Implemented favorites functionality in table

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { DataGrid, type GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data/people";
+import type { Person } from "@/models";
+import { Checkbox } from "@mui/material";
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
   const pageSize = 5;
 
   const [paginationModel, setPaginationModel] = useState({
@@ -12,12 +15,36 @@ const Home: React.FC<HomeInterface> = () => {
     pageSize: pageSize,
   });
 
+  const findPerson = (person: Person) =>
+    !!selectedPeople.find((p) => p.id === person.id);
+
+  const filterPerson = (person: Person) =>
+    selectedPeople.filter((p) => p.id !== person.id);
+
+  const handleChange = (person: Person) => {
+    setSelectedPeople(
+      findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
+    );
+  };
+
   const columns = [
     {
       field: "actions",
+      type: "actions",
+      sortable: false,
       headerName: "",
       width: 50,
-      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          {
+            <Checkbox
+              size="small"
+              checked={findPerson(params.row)}
+              onChange={() => handleChange(params.row)}
+            />
+          }
+        </>
+      ),
     },
     {
       field: "name",
