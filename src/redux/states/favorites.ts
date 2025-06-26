75213b7b61c9ea5f149c4ec643b9012394fb737e
Pablo del Barrio <pablodbb@gmail.com>
2025-06-26 13:31:51 +0200

    Implemented Redux and localstorage functionality

---
+++ src/redux/states/favorites.ts
@@ -0,0 +1,18 @@
+import { LocalStorageTypes, type Person } from "@/models";
+import { getLocalStorage, setLocalStorage } from "@/utilities";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: Person[] = [];
+
+export const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState: getLocalStorage(LocalStorageTypes.FAVORITES)
+    ? JSON.parse(getLocalStorage(LocalStorageTypes.FAVORITES) as string)
+    : initialState,
+  reducers: {
+    addFavorite: (state, action) => {
+      setLocalStorage(LocalStorageTypes.FAVORITES, state);
+      return action.payload;
+    },
+  },
+});
